5aa7a50d6652efc1a6f5a0338ce1ae51899d4037
PopeFrancisI <m.macieszczuk@gmail.com>
2016-10-29 18:16:15 +0200

    Failed enemy movement 1

---
+++ Assets/BasicEnemy_Controler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class BasicEnemy_Controler : MonoBehaviour {
+
+    public float velocity = 10;
+    CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+        cc.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        cc.Move(v * Time.deltaTime);
+        cc.SimpleMove(Physics.gravity);
+	}
+}
