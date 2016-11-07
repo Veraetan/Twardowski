df175e68a189462c8c269ea6f3b55b7d67b22d9f
PopeFrancisI <m.macieszczuk@gmail.com>
2016-11-07 14:45:42 +0100

    Revert "protoimp model and facing player code"
    This reverts commit 99cee2dac6efd4176a4ec71fbbbd3308922acb2e.

--- Assets/Scripts/BasicEnemy_Controler.cs
+++ Assets/Scripts/BasicEnemy_Controler.cs
@@ -25,6 +25,47 @@ public class BasicEnemy_Controler : MonoBehaviour {
         jump = false;
         
     }
+	/*
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float playerX = player.transform.position.x;
+        float enemyX = gameObject.transform.position.x;
+        bool playerOnTheRight = playerX > enemyX;
+
+        if (Mathf.Abs(playerX - enemyX) > 0.5f)
+        {
+            if (playerOnTheRight)
+            {
+                movement.x = runSpeed;
+            }
+            else
+            {
+                movement.x = -runSpeed;
+            }
+        }
+        else
+        {
+            movement.x = 0f;
+        }
+
+        var n = transform.position;
+        n.y += 1;
+        var heading = n - player.transform.position;
+        var distance = heading.magnitude;
+        var dir = heading / distance;
+        dir.y = 0;
+
+        Debug.DrawRay(n, -dir, Color.red, 5, true);
+
+        if (cc.isGrounded)
+        {
+
+        }
+
+        cc.Move(movement * Time.deltaTime);
+
+    }*/
 
     void FixedUpdate () {
 
@@ -32,31 +73,38 @@ public class BasicEnemy_Controler : MonoBehaviour {
         chaseDir.y = 0;
         float distance = chaseDir.magnitude;
 
+        //Debug.DrawRay(transform.position, chaseDir / chaseDir.magnitude, Color.red, 5, true);
+
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, chaseDir/chaseDir.magnitude, out hit, 1.5f))    //check if there is an obstacle on your way
+        if (Physics.Raycast(transform.position, chaseDir/chaseDir.magnitude, out hit, 1.5f))
         {
-            if(!hit.collider.gameObject.transform.IsChildOf(player.transform))  //if there is an obstacle - jump
+            if(!hit.collider.gameObject.transform.IsChildOf(player.transform))
                 jump = true;
         }
 
-        if (distance <= 1.5f)   //if the player is close...
+        if (distance <= 1.5f)
         {
-            if(player.GetComponent<PlayerController>().health >= 0) //... and the player is not dead...
-                player.GetComponent<PlayerController>().health -= 10;   //attack him
+            Debug.Log("Attacking Player");
 
-            //Debug.Log(player.GetComponent<PlayerController>().health);
+            //attacking player code
+
+            if(player.GetComponent<PlayerController>().health >= 0)
+                player.GetComponent<PlayerController>().health -= 10;
+
+            Debug.Log(player.GetComponent<PlayerController>().health);
 
             vSpeed -= gravity * Time.deltaTime;
+
             cc.Move(new Vector3(0f, vSpeed, 0f) * Time.deltaTime);
         }
         else
         {
-            if (cc.isGrounded)  //if monster is on the ground...
+            if (cc.isGrounded)
             {
                 vSpeed = 0;
-                if (jump)   //...and should jump...
+                if (jump)
                 {
-                    vSpeed = jumpSpeed; //then jump
+                    vSpeed = jumpSpeed;
                     jump = false;
                 }
             }
@@ -69,11 +117,8 @@ public class BasicEnemy_Controler : MonoBehaviour {
             cc.Move(chaseDir * Time.deltaTime);
         }
 
-        //face the player
-        var lookPos = player.transform.position - transform.position;
-        lookPos.y = 0;
-        var rotation = Quaternion.LookRotation(lookPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 180);
+
+        
     }
     
 }
