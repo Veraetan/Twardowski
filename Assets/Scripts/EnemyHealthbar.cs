da6f378039e362538e2dd7b2560e38284f8ade41
PopeFrancisI <m.macieszczuk@gmail.com>
2016-11-21 14:09:06 +0100

    protoEnemyHealthbar

---
+++ Assets/Scripts/EnemyHealthbar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealthbar : MonoBehaviour {
+
+    private float hp, maxhp;
+
+	// Use this for initialization
+	void Start () {
+        
+        hp = GetComponentInParent<CharController>().health;
+        maxhp = GetComponentInParent<CharController>().health_max;
+        GetComponent<TextMesh>().text = hp.ToString() + "/" + maxhp.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        hp = GetComponentInParent<CharController>().health;
+        maxhp = GetComponentInParent<CharController>().health_max;
+        GetComponent<TextMesh>().text = hp.ToString() + "/" + maxhp.ToString();
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0,180,0);
+
+    }
+}
