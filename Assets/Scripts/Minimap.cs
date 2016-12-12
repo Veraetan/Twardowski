ba6d268cc85538552e033f4a44615b34d80ad436
PopeFrancisI <m.macieszczuk@gmail.com>
2016-12-12 15:48:50 +0100

    minimap v0.5

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour
+{
+
+    GameObject player;
+    Vector3 diff;
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+        findPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            gameObject.transform.Translate(player.transform.position - diff);
+            diff = player.transform.position;
+        }
+    }
+
+    public void findPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        gameObject.transform.position = (player.transform.position + new Vector3(0, 0, -50));
+        diff = player.transform.position;
+    }
+    
+}
